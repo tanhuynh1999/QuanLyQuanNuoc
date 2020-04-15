949817f609dd84521d376809f95e989a4eae0d5b
Movie <55469056+TanThanh1902@users.noreply.github.com>
2020-04-16 00:10:23 +0700

    phanquyen

--- ThaiHoaiDu/Controllers/DangNhapController.cs
+++ ThaiHoaiDu/Controllers/DangNhapController.cs
@@ -22,11 +22,16 @@ namespace ThaiHoaiDu.Controllers
             string sTaiKhoan = f["taikhoan"].ToString();
             string sMatKhau = f["password"].ToString();
             NguoiDung tk = db.NguoiDungs.SingleOrDefault(n => n.TaiKhoan == sTaiKhoan && n.MatKhau == sMatKhau);
-            if (tk != null)
+            if (tk.VaiTro == true)
             {
                 Session["TaiKhoan"] = tk;
                 return Redirect("/QuanLy/Index");
             }
+            else if(tk.VaiTro == false)
+            {
+                Session["TaiKhoan"] = tk;
+                return Redirect("/QuanLy/Index");
+            }    
             else
             {
                 ViewBag.ThongBao = "Sai tài khoản hoặc mật khẩu!";
