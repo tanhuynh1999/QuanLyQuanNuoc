1c9660a7f2b43945256c704fc67b46b22fd40539
Movie <55469056+TanThanh1902@users.noreply.github.com>
2020-04-16 22:18:36 +0700

    thống kê

---
+++ ThaiHoaiDu/Controllers/ThongKeController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThaiHoaiDu.Models;
+
+namespace ThaiHoaiDu.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        QuanLyQuanNuocEntities db = new QuanLyQuanNuocEntities();
+        // GET: ThongKe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult tkNgay()
+        {
+            return PartialView(db.CTHDs.Where(t => DbFunctions.DiffDays(t.HoaDon.GioRa, DateTime.Now) <= 1 && t.HoaDon.GioRa != null).ToList());
+        }
+        public PartialViewResult tkTuan()
+        {
+            return PartialView(db.CTHDs.Where(t => DbFunctions.DiffDays(t.HoaDon.GioRa, DateTime.Now) <= 7 && t.HoaDon.GioRa != null).ToList());
+        }
+        public PartialViewResult tkThang()
+        {
+            return PartialView(db.CTHDs.Where(t => t.HoaDon.GioRa.Value.Month == DateTime.Now.Month && t.HoaDon.GioRa.Value.Year == DateTime.Now.Year && t.HoaDon.GioRa != null).ToList());
+        }
+        public PartialViewResult tkNam()
+        {
+            return PartialView(db.CTHDs.Where(t => t.HoaDon.GioRa.Value.Year == DateTime.Now.Year && t.HoaDon.GioRa != null).ToList());
+        }
+    }
+}
