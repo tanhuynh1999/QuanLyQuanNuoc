7106690c8d7cfaece6230c8674a7fe942423edd3
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2020-04-18 03:10:40 +0700

    mới nhất

---
+++ ThaiHoaiDu/Controllers/ThongBao_HienThiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThaiHoaiDu.Controllers
+{
+    public class ThongBao_HienThiController : Controller
+    {
+        // GET: ThongBao_HienThi
+        public PartialViewResult HuongDan()
+        {
+            return PartialView();
+        }
+    }
+}
